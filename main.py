7eaa338d0f1897457d749b74d34bf1f0013df45b
hackartists <hackartists@gmail.com>
2023-04-16 20:46:26 +0900

    network

--- main.py
+++ main.py
@@ -76,35 +76,47 @@ max_topics=20
 
 
 for (g,s,e) in generations:
-    for base in ['raw-data']:
-        start_date = s
-        end_date = e
+    for base in ['reddit', 'raw-data']:
+        n=network.Network(s,e,base,rank=100)
+        n.load()
+        n.save()
+        n.make_graph_from_base()
+        print(f'Finished making a top-100 graph from {s} to {e}')
 
-        month = start_date % 100
-        end_month = end_date % 100
-        year = int(start_date / 100)
-        date = start_date
-        lines = ""
-        while date <= end_date:
-            n=network.Network(date,date,base)
-            n.load_from_files()
-            n.make_graph()
-            print(f'Finished making a graph for {date}')
+        n=network.Network(s,e,base,rank=30)
+        n.load()
+        n.save()
+        n.make_graph_from_base()
+        print(f'Finished making a top-30 graph from {s} to {e}')
 
-            month = month + 1
-            if month == 13:
-                month = 1
-                year = year + 1
-            date = (year*100) + month
-            print(f'Finished making a graph for {base}')
+        # start_date = s
+        # end_date = e
+
+        # month = start_date % 100
+        # end_month = end_date % 100
+        # year = int(start_date / 100)
+        # date = start_date
+        # lines = ""
+        # while date <= end_date:
+        #     n=network.Network(date,date,base)
+        #     n.load_from_files()
+        #     n.make_graph()
+        #     print(f'Finished making a graph for {date}')
+
+        #     month = month + 1
+        #     if month == 13:
+        #         month = 1
+        #         year = year + 1
+        #     date = (year*100) + month
+        #     print(f'Finished making a graph for {base}')
 
         # f = frequency.FrequencyMining(s, e, base)
         # f.run(f'output/freq-{base}-{g}.csv')
         # print(f'{g}: frequency has been completed.\n')
 
-        l = lda.LdaTopicModeling(s, e, base)
-        l = l.run(min_topics,max_topics, f'output/lda-{base}-{g}')
-        print(f'{g}: LDA topic modeling has been completed.\n')
+        # l = lda.LdaTopicModeling(s, e, base)
+        # l = l.run(min_topics,max_topics, f'output/lda-{base}-{g}')
+        # print(f'{g}: LDA topic modeling has been completed.\n')
 
 # r = reddit.Reddit(201604, 202212, "dao")
 # r.toCsv()
--- network.py
+++ network.py
@@ -12,69 +12,79 @@ logging.basicConfig(
 
 
 class Network(lda.LdaTopicModeling):
-    def __init__(self, start_date, end_date, base="raw-data"):
+    def __init__(self, start_date, end_date, base="raw-data", rank=100):
         self.start_date = start_date
         self.end_date = end_date
         self.text = ""
         self.base = base
+        self.rank = rank
         if base == 'raw-data':
-            self.filter = tp.twitter
+            self.filter = tp.dict_by_rank('twitter', self.rank)
         else:
-            self.filter = tp.reddit_top
+            self.filter = tp.dict_by_rank('reddit', self.rank)
         super().__init__(start_date, end_date, base)
 
     def load_from_base(self):
         f = open(f'network-data/{self.base}.txt', 'r')
-        self.text = f.read().split('\n')
+        self.texts = f.read().split('\n')
         f.close()
+        if self.rank != 100:
+            self.texts = [self.preprocessing(t) for t in self.texts]
+            self.save()
 
-        return self.text
+        return self.texts
 
     def load_from_files(self):
         self.load()
         self.preprocessing()
 
-    # def load(self):
-    #     start_date = self.start_date
-    #     end_date = self.end_date
+    def load(self):
+        start_date = self.start_date
+        end_date = self.end_date
 
-    #     month = start_date % 100
-    #     year = int(start_date / 100)
-    #     date = start_date
-    #     lines = []
-    #     while date <= end_date:
-    #         filename = "{2}/{0}-{1:02d}.txt".format(year,month,self.base)
-    #         f = open(filename,"r")
-    #         for l in f.read().split('\n'):
-    #             l = self.preprocessing(l)
-    #             if l != '' and ' ' in l:
-    #                 lines = lines + [l]
-    #         f.close()
+        month = start_date % 100
+        year = int(start_date / 100)
+        date = start_date
+        lines = []
+        while date <= end_date:
+            filename = "{2}/{0}-{1:02d}.txt".format(year,month,self.base)
+            f = open(filename,"r")
+            for l in f.read().split('\n'):
+                l = self.preprocessing(l)
+                if l != '' and ' ' in l:
+                    lines = lines + [l]
+            f.close()
 
-    #         month = month + 1
-    #         if month == 13:
-    #             month = 1
-    #             year = year + 1
-    #         date = (year*100) + month
+            month = month + 1
+            if month == 13:
+                month = 1
+                year = year + 1
+            date = (year*100) + month
 
-    #     self.text = lines
+        self.texts = lines
 
-    #     return lines
+        return lines
+
+    def preprocessing(self, text):
+        t = self.preproc_line(text)
+        return ' '.join([ w for w in t.split(' ') if w in self.filter and self.filter[w] ]) 
 
     def save(self):
-        f = open(f'{self.base}.txt', 'w')
-        f.write('\n'.join(self.text))
+        f = open(f'network-data/{self.base}-{self.rank}.txt', 'w')
+        f.write('\n'.join(self.texts))
         f.close()
 
-    def make_graph(self):
-        self.prepare()
-        self.texts = [' '.join(text) for text in self.texts]
-        corpus = tn.Corpus(pd.Series(self.texts, dtype='float64'))
-        t = tn.Textnet(corpus.tokenized())
+    def make_graph_from_base(self):
+        corpus = tn.Corpus(pd.Series(self.texts))
+        t = tn.Textnet(corpus.tokenized(stem=False))
         print('completed tokenization')
         words = t.project(node_type="term")
         g = words.graph
         g.vs["label"] = g.vs["id"]
         print('it will make a graph model')
-        g.write_gml('network-data/{0}-{1}-{2}.gml'.format(self.base, self.start_date, self.end_date))
-        # words.save_graph(target=f'network-data/{self.base}.gml')
+        g.write_gml('network-data/{0}-top-{1}.gml'.format(self.base, self.rank))
+
+    def make_graph(self):
+        self.prepare()
+        self.texts = [' '.join(text) for text in self.texts]
+        self.make_graph_from_base()
